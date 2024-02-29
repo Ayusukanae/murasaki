c978a9c524b00e00ff7c9a910e353168a5d0bae4
annil-trem <carlosgame463@gmail.com>
2024-02-29 14:49:37 -0300

    gg

---
+++ cadastro.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>cadastro</title>
+</head>
+<body>
+    <div class="box">
+        <form action="">
+            <fieldset>
+                <legend><b>Formulario de Clientes</b></legend>
+                <br>
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
