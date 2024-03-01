15eb85f43f30dd8309cdef09b682cf6099d9c996
annil-trem <carlosgame463@gmail.com>
2024-03-01 10:27:54 -0300

    cads

--- cadastro.php
+++ cadastro.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   
+<link rel="stylesheet" href="css/cadastro.css">
     <title>cadastro</title>
 </head>
+
 <body>
     <div class="box">
         <form action="">
@@ -11,10 +13,50 @@
                 <legend><b>Formulario de Clientes</b></legend>
                 <br>
                 <div class="inputbox>">
-                    <input type="text" name="nome" id="nome"class
+                    <input type="text" name="nome" id="nome" class="inputUser" required>
+                    <label for="none" class="labelinput">nome completo</label>
                 </div>
-            </fieldset>
-        </form>
+                <br><br>
+                <div class="inputbox>">
+                    <input type="text" name="email" id="email" class="inputUser" required>
+                    <label for="none" class="labelinput">email</label>
+                </div>
+                <br><br>
+                <div class="inputbox>">
+                    <input type="tel" name="telefone" id="telefone" class="inputUser" required>
+                    <label for="none" class="labelinput">telefone</label>
+                </div>
+                <br><br>
+                <div class="inputbox>">
+                    <input type="date" name="data_nascimento" id="data de nascimento" class="inputUser" required>
+                    <label for="none" class="labelinput">data de nascimento</label>
+                </div>
+                <br><br>
+                <div class="inputbox>">
+                    <input type="text" name="cidade" id="cidade" class="inputUser" required>
+                    <label for="none" class="labelinput">cidade</label>
+                </div>
+                <br><br>
+                <div class="inputbox>">
+                    <input type="text" name="estado" id="estado" class="inputUser" required>
+                    <label for="none" class="labelinput">estado</label>
+                </div>
+                <br><br>
+                <div class="inputbox>">
+                    <input type="text" name="endereco" id="endereco" class="inputUser" required>
+                    <label for="none" class="labelinput">endereço</label>
+                </div>
+                <br><br>
+                <div class="inputbox>">
+                    <input type="password" name="senha" id="senha" class="inputUser" required>
+                    <label for="none" class="labelinput">senha</label>
+                </div>
+                <br><br>
+                <input type="submit" nome="submit" id="submit">
+    </div>
+    </fieldset>
+    </form>
     </div>
 </body>
+
 </html>
